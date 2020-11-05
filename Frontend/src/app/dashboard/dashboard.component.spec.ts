b23053ffbe41015d4b0ce1cac2a8fe794dcdb178
Rinku Yadav <42610036+rinkuyadav8955@users.noreply.github.com>
2020-11-05 11:40:04 +0530

    Update dashboard.component.spec.ts

--- Frontend/src/app/dashboard/dashboard.component.spec.ts
+++ Frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -11,6 +11,7 @@ describe('DashboardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [        
         RouterModule.forRoot([]),
+        HttpClientTestingModule,
     ],
       declarations: [ DashboardComponent ]
     })

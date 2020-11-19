b2af646b2aaced2e67cdda9b8a9cc96e1ad84699
Rinku Yadav <42610036+rinkuyadav8955@users.noreply.github.com>
2020-11-19 10:08:34 +0530

    Update performance-chart.component.ts
    Add documentation

--- Frontend/src/app/performance-chart.component.ts
+++ Frontend/src/app/performance-chart.component.ts
@@ -3,7 +3,9 @@ import * as CanvasJS from 'canvasjs.min.js';
 import { Component, OnInit, Input, AfterViewInit, ViewChild  } from '@angular/core';
 import {Router,ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
-
+  /**
+  * This component enables the user to anyalsis the performance using point vs count graph.
+  */
 @Component({​​​​
 
   selector: 'app-performance-chart',
@@ -25,7 +27,10 @@ export class PerformanceChartComponent implements OnInit {​​​​
   constructor(private performanceService:PerformanceChartService,private router: Router,private route:ActivatedRoute,private _location: Location) {​​​​ }​​​​
 
 
-
+  /**
+  * This intialize the Quiz id from URL.
+  * It takes quiz data from service and initialize the data into quiz variable.
+  */
   ngOnInit() {
   let id = parseInt(this.route.snapshot.paramMap.get('id'));
    this.quizId = id;
@@ -71,9 +76,16 @@ export class PerformanceChartComponent implements OnInit {​​​​
 
 
   }​​​​
+    /**
+  * This method will call when user clicking on back button.
+  * it route the user into previous page.
+  */
    backClicked() {
     this._location.back();
   }
+    /**
+  * This Method draw the point vs count graph on the basis of respective data.
+  */
   sampleData(result,quiz)
   { 
      var max_score = this.quiz.questions.length;
@@ -100,6 +112,10 @@ export class PerformanceChartComponent implements OnInit {​​​​
   }
 
 }​​​​
+
+  /**
+  * This is interface to store quiz data to draw the graph.
+  */
 export interface UserData {
   y: number;
   label:string;
